98c4ee111ad2e76e105807b59fd6179e1362e36b
unknown <lefta.chandra@gmail.com>
2026-01-26 15:17:46 +0900

    add plugin group

--- kantong_buku.plugin.php
+++ kantong_buku.plugin.php
@@ -12,4 +12,10 @@ use SLiMS\Plugins;
 $plugin = Plugins::getInstance();
 
 Plugins::getInstance()->registerAutoload(__DIR__);
-Plugins::menu('bibliography', 'Kantong Buku', __DIR__ . '/pages/book_pocket.php');
+
+$path =  __DIR__ . '/pages/book_pocket.php';
+ // Make default group menu
+Plugins::group('Kelengkapan Bahan Pustaka', function() use($path) {
+            // Scan all file inside module directory as menu
+Plugins::menu('bibliography', 'Kantong Buku', $path);
+});
